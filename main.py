842be096267b5e45ce50d66d4385f67cfdd4ba47
Diego <diegomag2996@gmail.com>
2025-04-06 01:01:23 -0600

    cambio de color general

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from views.login import login_view
 
 def main(page: ft.Page):
     page.title = "Consultorio Dental"
-    page.theme_mode = ft.ThemeMode.DARK
+    page.theme_mode = ft.ThemeMode.LIGHT
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     login_view(page)  # Carga la vista principal (login)
 
